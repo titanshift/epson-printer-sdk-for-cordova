06202c0a59a65c3681415f629633fa15687ebb4f
Ahmed Ouadi <accoladescripts@gmail.com>
2014-05-26 22:21:23 +0100

    fix a path

--- src/android/Printer.java
+++ src/android/Printer.java
@@ -9,8 +9,21 @@ import org.json.JSONException;
 
 public class Printer extends CordovaPlugin {
 
-	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-	 
-	}
+	public static final String ECHO = "echo";
+    
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        try {
+            if (ECHO.equals(action)) { 
+                
+               return true;
+            }
+            callbackContext.error("Invalid action");
+            return false;
+        } catch(Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+            callbackContext.error(e.getMessage());
+            return false;
+        } 
+    }
 }
